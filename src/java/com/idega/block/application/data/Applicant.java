c407132f0d4ccd2190a163d70c86cc7677d99393
Aron Birkir Gudmundsson <aron@idega.com>
2001-08-01 11:17:37 +0000

    no message

--- src/java/com/idega/block/application/data/Applicant.java
+++ src/java/com/idega/block/application/data/Applicant.java
@@ -1,5 +1,5 @@
 /*
- * $Id: Applicant.java,v 1.8 2001/07/23 11:02:55 aron Exp $
+ * $Id: Applicant.java,v 1.9 2001/08/01 11:17:37 aron Exp $
  *
  * Copyright (C) 2001 Idega hf. All Rights Reserved.
  *
@@ -40,71 +40,30 @@ public class Applicant extends GenericEntity {
 
   public void initializeAttributes() {
     addAttribute(getIDColumnName());
-    addAttribute(firstName_,"First name",true,true,"java.lang.String");
-    addAttribute(middleName_,"Middle name",true,true,"java.lang.String");
-    addAttribute(lastName_,"Last name",true,true,"java.lang.String");
-    addAttribute(ssn_,"Social security number",true,true,"java.lang.String");
-    addAttribute(legalResidence_,"Legal residence",true,true,"java.lang.String");
-    addAttribute(residence_,"Residence",true,true,"java.lang.String");
-    addAttribute(residencePhone_,"Telephone",true,true,"java.lang.String");
-    addAttribute(po_,"Post office",true,true,"java.lang.String");
-    addAttribute(mobilePhone_,"Mobile phone",true,true,"java.lang.String");
-    addAttribute(sendSMS_,"Send SMS",true,true,"java.lang.String");
-    setMaxLength(firstName_,255);
-    setMaxLength(middleName_,255);
-    setMaxLength(lastName_,255);
-    setMaxLength(ssn_,20);
-    setMaxLength(legalResidence_,255);
-    setMaxLength(residence_,255);
-    setMaxLength(residencePhone_,40);
-    setMaxLength(po_,255);
-    setMaxLength(mobilePhone_,40);
-    setMaxLength(sendSMS_,1);
+    addAttribute(firstName_,"First name",true,true,"java.lang.String",255);
+    addAttribute(middleName_,"Middle name",true,true,"java.lang.String",255);
+    addAttribute(lastName_,"Last name",true,true,"java.lang.String",255);
+    addAttribute(ssn_,"Social security number",true,true,"java.lang.String",20);
+    addAttribute(legalResidence_,"Legal residence",true,true,"java.lang.String",255);
+    addAttribute(residence_,"Residence",true,true,"java.lang.String",255);
+    addAttribute(residencePhone_,"Telephone",true,true,"java.lang.String",40);
+    addAttribute(po_,"Post office",true,true,"java.lang.String",255);
+    addAttribute(mobilePhone_,"Mobile phone",true,true,"java.lang.String",40);
+    addAttribute(sendSMS_,"Send SMS",true,true,"java.lang.String",1);
   }
 
-  public String getEntityName() {
-    return(name_);
-  }
-
-  public String getFirstNameColumnName() {
-    return(firstName_);
-  }
-
-  public String getMiddleNameColumnName() {
-    return(middleName_);
-  }
-
-  public String getLastNameColumnName() {
-    return(lastName_);
-  }
-
-  public String getSSNColumnName() {
-    return(ssn_);
-  }
-
-  public String getLegalResidenceColumnName() {
-    return(legalResidence_);
-  }
-
-  public String getResidenceColumnName() {
-    return(residence_);
-  }
-
-  public String getResidencePhoneColumnName() {
-    return(residencePhone_);
-  }
-
-  public String getPOColumnName() {
-    return(po_);
-  }
-
-  public String getMobilePhoneColumnName() {
-    return(mobilePhone_);
-  }
-
-  public String getSendSMSColumnName() {
-    return(sendSMS_);
-  }
+  public String getEntityName() {    return(name_);  }
+  public static String getEntityTableName() {    return(name_);  }
+  public static String getFirstNameColumnName() {    return(firstName_);  }
+  public static String getMiddleNameColumnName() {    return(middleName_);  }
+  public static String getLastNameColumnName() {    return(lastName_);  }
+  public static String getSSNColumnName() {    return(ssn_);  }
+  public static String getLegalResidenceColumnName() {    return(legalResidence_);  }
+  public static String getResidenceColumnName() {    return(residence_);  }
+  public static String getResidencePhoneColumnName() {    return(residencePhone_);  }
+  public static String getPOColumnName() {    return(po_);  }
+  public static String getMobilePhoneColumnName() {    return(mobilePhone_);  }
+  public static String getSendSMSColumnName() {    return(sendSMS_);  }
 
   public void setFirstName(String name) {
     setColumn(firstName_,name);
