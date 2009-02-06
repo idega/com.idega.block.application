147838fa2d1b468843520a3ed3c6ee5022908f9a
Pall Helgason <palli@idega.com>
2009-02-06 15:43:54 +0000

    Lots of FS stuff

--- src/java/com/idega/block/application/presentation/ApplicationForm.java
+++ src/java/com/idega/block/application/presentation/ApplicationForm.java
@@ -1,5 +1,5 @@
 /*
- * $Id: ApplicationForm.java,v 1.25.2.2 2007/01/12 19:31:19 idegaweb Exp $
+ * $Id: ApplicationForm.java,v 1.25.2.3 2009/02/06 15:43:54 palli Exp $
  *
  * Copyright (C) 2001 Idega hf. All Rights Reserved.
  *
@@ -50,40 +50,39 @@ public class ApplicationForm extends PresentationObjectContainer {
 	public static final String APP_PO = "app_po";
 	public static final String APP_STATUS = "app_status";
 	public static final String APP_MUST_FILL_OUT = "app_mustFillOut";
-	protected final int _statusEnteringPage = 0;
-	protected final int _statusSubject = 1;
-	protected final int _statusGeneralInfo = 2;
+	
+	protected static final int STATUS_ENTERING_PAGE = 0;
+	protected static final int STATUS_SUBJECT = 1;
+	protected static final int STATUS_GENERAL_INFO = 2;
+	protected static final Text REQUIRED = new Text(" * ");
+	
 	private static final String IW_RESOURCE_BUNDLE = "com.idega.block.application";
-	protected IWResourceBundle _iwrb = null;
-	protected Text _required = new Text(" * ");
-	protected Text _info = null;
-	/**
-	 *
-	 */
+
+	protected IWResourceBundle iwrb = null;
+	protected Text info = null;
+	
 	public ApplicationForm() {
 	}
-	/**
-	 *
-	 */
+
 	protected void control(IWContext iwc) {
 		debugParameters(iwc);
 		String statusString = iwc.getParameter(APP_STATUS);
 		int status = 0;
 		if (statusString == null) {
-			status = this._statusEnteringPage;
+			status = this.STATUS_ENTERING_PAGE;
 		}
 		else {
 			try {
 				status = Integer.parseInt(statusString);
 			}
 			catch (NumberFormatException e) {
-				status = this._statusEnteringPage;
+				status = this.STATUS_ENTERING_PAGE;
 			}
 		}
-		if (status == this._statusEnteringPage) {
+		if (status == this.STATUS_ENTERING_PAGE) {
 			doSelectSubject();
 		}
-		else if (status == this._statusSubject) {
+		else if (status == this.STATUS_SUBJECT) {
 			try {
 				ApplicationFormHelper.saveSubject(iwc);
 			}
@@ -95,7 +94,7 @@ public class ApplicationForm extends PresentationObjectContainer {
 			}
 			doGeneralInformation();
 		}
-		else if (status == this._statusGeneralInfo) {
+		else if (status == this.STATUS_GENERAL_INFO) {
 			try {
 				ApplicationFormHelper.saveApplicantInformation(iwc);
 			}
@@ -135,29 +134,29 @@ public class ApplicationForm extends PresentationObjectContainer {
 		t.setWidth(2, "50%");
 		Text heading = (Text) textTemplate.clone();
 		heading.setStyle("headlinetext");
-		heading.setText(this._iwrb.getLocalizedString("applicationSubjectTitle", "Veldu tegund ums?knar"));
+		heading.setText(this.iwrb.getLocalizedString("applicationSubjectTitle", "Veldu tegund ums?knar"));
 		Text text1 = (Text) textTemplate.clone();
 		text1.setStyle("bodytext");
-		text1.setText(this._iwrb.getLocalizedString("applicationSubject", "Ums?kn um"));
+		text1.setText(this.iwrb.getLocalizedString("applicationSubject", "Ums?kn um"));
 		text1.setBold();
 		DropdownMenu subject = new DropdownMenu(subjects, "subject");
 		subject.setStyleClass("formstyle");
-		BackButton back = new BackButton(this._iwrb.getImage("back.gif"));
-		SubmitButton ok = new SubmitButton(this._iwrb.getImage("next.gif", this._iwrb.getLocalizedString("ok", "?fram")));
+		BackButton back = new BackButton(this.iwrb.getImage("back.gif"));
+		SubmitButton ok = new SubmitButton(this.iwrb.getImage("next.gif", this.iwrb.getLocalizedString("ok", "?fram")));
 		form.add(heading);
 		form.add(Text.getBreak());
 		form.add(Text.getBreak());
 		form.add(t);
 		t.add(text1, 1, 1);
-		t.add(this._required, 1, 1);
+		t.add(this.REQUIRED, 1, 1);
 		t.add(subject, 2, 1);
 		t.add(back, 2, 3);
 		t.add(ok, 2, 3);
 		form.add(Text.getBreak());
 		form.add(Text.getBreak());
 		form.add(Text.getBreak());
-		form.add(this._info);
-		form.add(new HiddenInput(APP_STATUS, Integer.toString(this._statusSubject)));
+		form.add(this.info);
+		form.add(new HiddenInput(APP_STATUS, Integer.toString(this.STATUS_SUBJECT)));
 		add(form);
 	}
 	protected void doGeneralInformation() {
@@ -165,40 +164,40 @@ public class ApplicationForm extends PresentationObjectContainer {
 		TextInput textInputTemplate = new TextInput();
 		Form form = new Form();
 		DataTable t = new DataTable();
-		SubmitButton ok = new SubmitButton(this._iwrb.getImage("next.gif", this._iwrb.getLocalizedString("ok", "?fram")));
+		SubmitButton ok = new SubmitButton(this.iwrb.getImage("next.gif", this.iwrb.getLocalizedString("ok", "?fram")));
 		Text heading = (Text) textTemplate.clone();
 		heading.setStyle("headlinetext");
-		heading.setText(this._iwrb.getLocalizedString("generalInfo", "Almennar uppl?singar um ums?kjanda"));
+		heading.setText(this.iwrb.getLocalizedString("generalInfo", "Almennar uppl?singar um ums?kjanda"));
 		Text text1 = (Text) textTemplate.clone();
 		text1.setStyle("bodytext");
-		text1.setText(this._iwrb.getLocalizedString("firstName", "Fornafn"));
+		text1.setText(this.iwrb.getLocalizedString("firstName", "Fornafn"));
 		text1.setBold();
 		Text text2 = (Text) textTemplate.clone();
 		text2.setStyle("bodytext");
-		text2.setText(this._iwrb.getLocalizedString("middleName", "Millinafn"));
+		text2.setText(this.iwrb.getLocalizedString("middleName", "Millinafn"));
 		Text text3 = (Text) textTemplate.clone();
 		text3.setStyle("bodytext");
-		text3.setText(this._iwrb.getLocalizedString("lastName", "Eftirnafn"));
+		text3.setText(this.iwrb.getLocalizedString("lastName", "Eftirnafn"));
 		text3.setBold();
 		Text text4 = (Text) textTemplate.clone();
 		text4.setStyle("bodytext");
-		text4.setText(this._iwrb.getLocalizedString("ssn", "Kennitala"));
+		text4.setText(this.iwrb.getLocalizedString("ssn", "Kennitala"));
 		text4.setBold();
 		Text text5 = (Text) textTemplate.clone();
 		text5.setStyle("bodytext");
-		text5.setText(this._iwrb.getLocalizedString("legalResidence", "L?gheimili"));
+		text5.setText(this.iwrb.getLocalizedString("legalResidence", "L?gheimili"));
 		text5.setBold();
 		Text text6 = (Text) textTemplate.clone();
 		text6.setStyle("bodytext");
-		text6.setText(this._iwrb.getLocalizedString("residence", "Dvalarsta?ur"));
+		text6.setText(this.iwrb.getLocalizedString("residence", "Dvalarsta?ur"));
 		text6.setBold();
 		Text text7 = (Text) textTemplate.clone();
 		text7.setStyle("bodytext");
-		text7.setText(this._iwrb.getLocalizedString("residencePhone", "S?man?mer ? dvalarsta?"));
+		text7.setText(this.iwrb.getLocalizedString("residencePhone", "S?man?mer ? dvalarsta?"));
 		text7.setBold();
 		Text text8 = (Text) textTemplate.clone();
 		text8.setStyle("bodytext");
-		text8.setText(this._iwrb.getLocalizedString("po", "P?stn?mer"));
+		text8.setText(this.iwrb.getLocalizedString("po", "P?stn?mer"));
 		text8.setBold();
 		TextInput input1 = (TextInput) textInputTemplate.clone();
 		input1.setName("firstName");
@@ -234,66 +233,66 @@ public class ApplicationForm extends PresentationObjectContainer {
 		input8.setStyleClass("formstyle");
 		t.addTitle(heading);
 		t.add(text1, 1, 1);
-		t.add(this._required, 1, 1);
+		t.add(this.REQUIRED, 1, 1);
 		t.add(input1, 2, 1);
 		t.add(text2, 1, 2);
 		t.add(input2, 2, 2);
 		t.add(text3, 1, 3);
-		t.add(this._required, 1, 3);
+		t.add(this.REQUIRED, 1, 3);
 		t.add(input3, 2, 3);
 		t.add(text4, 1, 4);
-		t.add(this._required, 1, 4);
+		t.add(this.REQUIRED, 1, 4);
 		t.add(input4, 2, 4);
 		t.add(text5, 1, 5);
-		t.add(this._required, 1, 5);
+		t.add(this.REQUIRED, 1, 5);
 		t.add(input5, 2, 5);
 		t.add(text6, 1, 6);
-		t.add(this._required, 1, 6);
+		t.add(this.REQUIRED, 1, 6);
 		t.add(input6, 2, 6);
 		t.add(text7, 1, 7);
-		t.add(this._required, 1, 7);
+		t.add(this.REQUIRED, 1, 7);
 		t.add(input7, 2, 7);
 		t.add(text8, 1, 8);
-		t.add(this._required, 1, 8);
+		t.add(this.REQUIRED, 1, 8);
 		t.add(input8, 2, 8);
 		t.addButton(ok);
 		form.add(t);
 		form.add(Text.getBreak());
 		form.add(Text.getBreak());
 		form.add(Text.getBreak());
-		form.add(this._info);
-		form.add(new HiddenInput(APP_STATUS, Integer.toString(this._statusGeneralInfo)));
+		form.add(this.info);
+		form.add(new HiddenInput(APP_STATUS, Integer.toString(this.STATUS_GENERAL_INFO)));
 		add(form);
 	}
 	protected void doDone() {
-		add(this._iwrb.getLocalizedString("applicationRegistered", "Ums?kn skr??"));
+		add(this.iwrb.getLocalizedString("applicationRegistered", "Ums?kn skr??"));
 	}
 	protected void doDone(String cypher) {
 		Text cypherText = new Text(cypher);
 		cypherText.setBold();
 		add(
-			this._iwrb.getLocalizedString("applicationRegistered", "Ums?kn skr??")
+			this.iwrb.getLocalizedString("applicationRegistered", "Ums?kn skr??")
 				+ ". "
-				+ this._iwrb.getLocalizedString("applicationReferenceNumber", "Tilv?sunarn?mer ?itt er")
+				+ this.iwrb.getLocalizedString("applicationReferenceNumber", "Tilv?sunarn?mer ?itt er")
 				+ ": ");
 		add(cypherText);
 		add(Text.getBreak());
 		add(Text.getBreak());
 		add(
-			this._iwrb.getLocalizedString(
+			this.iwrb.getLocalizedString(
 				"applicationFollowUp",
 				"?? getur nota? tilv?sunarn?meri? til a? fylgjast me? st??u ums?knar ?innar")
 				+ ".");
 	}
 	protected void doError() {
-		add(this._iwrb.getLocalizedString("applicationDBError", "Gagnagrunnsvilla vi? skr?ningu ums?knar"));
+		add(this.iwrb.getLocalizedString("applicationDBError", "Gagnagrunnsvilla vi? skr?ningu ums?knar"));
 	}
 	public void main(IWContext iwc) {
-		this._iwrb = getResourceBundle(iwc);
-		this._info = new Text(this._iwrb.getLocalizedString(APP_MUST_FILL_OUT, "* Stj?rnumerkt sv??i ver?ur a? fylla ?t"));
+		this.iwrb = getResourceBundle(iwc);
+		this.info = new Text(this.iwrb.getLocalizedString(APP_MUST_FILL_OUT, "* Stj?rnumerkt sv??i ver?ur a? fylla ?t"));
 		control(iwc);
 	}
 	protected void doSubjectError() {
-		add(this._iwrb.getLocalizedString("applicationSubjectError", "Nothing to apply for"));
+		add(this.iwrb.getLocalizedString("applicationSubjectError", "Nothing to apply for"));
 	}
 }
